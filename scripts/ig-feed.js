3eac55839a07e0bed05ecb3c3c0542e29c5c2138
Mamadou Coulibaly <mamadoucoulibaly@Mamadous-MacBook-Pro.local>
2020-07-03 22:39:34 -0400

    Completed instagram feed

---
+++ scripts/ig-feed.js
@@ -0,0 +1,7 @@
+    var i, e, d = document,
+        s = "script";
+    i = d.createElement("script");
+    i.async = 1;
+    i.src = "https://cdn.curator.io/published/f514e377-8815-4cbf-938a-c00340887c10.js";
+    e = d.getElementsByTagName(s)[0];
+    e.parentNode.insertBefore(i, e);
