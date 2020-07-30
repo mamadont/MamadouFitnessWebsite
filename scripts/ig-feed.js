f3af83d018bfc89ec8168e4c3ccb312e628349b1
mcoulibaly11 <mcoulibaly@graham-tech.net>
2020-07-30 15:06:58 -0400

    Added instagram feed

--- scripts/ig-feed.js
+++
@@ -1,7 +0,0 @@
-    var i, e, d = document,
-        s = "script";
-    i = d.createElement("script");
-    i.async = 1;
-    i.src = "https://cdn.curator.io/published/f514e377-8815-4cbf-938a-c00340887c10.js";
-    e = d.getElementsByTagName(s)[0];
-    e.parentNode.insertBefore(i, e);
