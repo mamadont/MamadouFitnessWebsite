0c81b8ecf423752a9409c97a161070fa4a7beaac
mcoulibaly11 <mcoulibaly@graham-tech.net>
2020-07-30 16:19:03 -0400

    Auto scaling height for social media banner

--- .history/scripts/script_20200625201624.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-    console.log(window.innerWidth)
-}
-
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}
---
+++ .history/scripts/script_20200730152403.js
@@ -0,0 +1,8 @@
+$(".navbar-nav ul li a").on("click", function(e) {
+    // 1
+    e.preventDefault();
+    // 2
+    const href = $(this).attr("href");
+    // 3
+    $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+});
